1ceef39984a6043aba381671bf7b90cb1f446acf
bibindon <bibindon0814@gmail.com>
2025-04-22 23:44:09 +0900

    j

--- command/command.cpp
+++ command/command.cpp
@@ -79,7 +79,8 @@ void CommandLib::Draw()
     // もし、選択不可能なコマンド上にカーソルがあったら、
     // 選択可能なコマンド上にカーソルを移動させる。
 
-    if (!m_commandList.at(m_cursorIndex).GetEnable())
+    if (m_commandList.size() <= m_cursorIndex ||
+        !m_commandList.at(m_cursorIndex).GetEnable())
     {
         m_cursorIndex = 0;
 
